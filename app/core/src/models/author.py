637cf577bfbf391dbb45fd7d2c2b0fef20a8a503
Herman <hermanx97d@gmail.com>
2024-10-07 23:57:49 -0500

    fixing flake8 problems

--- app/core/src/models/author.py
+++ app/core/src/models/author.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 
+
 class Author(BaseModel):
     id: int
     name: str
--- app/core/src/models/book.py
+++ app/core/src/models/book.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel
 from typing import Optional
 
+
 class Book(BaseModel):
     id: int
     title: str
